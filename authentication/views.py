0c345d9183e4907431c0eea8285ce82ebf9e2058
Mukhammadkodir Abdusalomov <mukhammadkodirabdusalomov27@gmail.com>
2024-03-27 23:29:14 +0100

    Message Alerts Continue...

--- authentication/views.py
+++ authentication/views.py
@@ -4,6 +4,7 @@ import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from validate_email import validate_email
+from django.contrib import messages
 
 
 class EmailValidationView(View):
@@ -31,4 +32,10 @@ class UsernameValidationView(View):
 
 class RegistrationView(View):
     def get(self, request):
+        return render(request, "authentication/register.html")
+
+    def post(self, request):
+
+        messages.success(request, "Success Whatsapp")
+
         return render(request, "authentication/register.html")
