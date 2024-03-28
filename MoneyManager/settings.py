1603495fa05320dd9949bc4b3d17fca28cbb8fc7
Mukhammadkodir Abdusalomov <mukhammadkodirabdusalomov27@gmail.com>
2024-03-28 23:00:30 +0100

    Email Send Success

--- MoneyManager/settings.py
+++ MoneyManager/settings.py
@@ -125,3 +125,13 @@ django_heroku.settings(locals())
 MESSAGE_TAGS = {
     messages.ERROR: "danger"
 }
+
+
+# ! email stuff
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "mypythonproject27@gmail.com"
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "mypythonproject27@gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = "ttyg wyik tifd sufu"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- authentication/views.py
+++ authentication/views.py
@@ -9,6 +9,7 @@ from django.core.mail import send_mail
 from django.core.mail import EmailMessage
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.conf import settings
 
 
 class EmailValidationView(View):
@@ -58,15 +59,17 @@ class RegistrationView(View):
 
                 user = User.objects.create_user(username=username, email=email)
                 user.set_password(password)
+                user.is_active = False
                 user.save()
-               # email_subject = "Activate your account"
-               #  email_body = ""
-               # email = EmailMessage{
-               #     email_subject,
-               #      email_body,
-               #      "noreply@semicolon.com",
-               #      [email],
-               #   }
+                email_subject = "Activate your account"
+                email_body = "Test Body"
+                email = EmailMessage(
+                    email_subject,
+                    email_body,
+                    "noreply@semicolon.com",
+                    [email],
+                )
+                email.send(fail_silently=False)
                 messages.success(request, "Account successfully created")
                 return render(request, "authentication/register.html")
 
