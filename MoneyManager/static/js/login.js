b585cee636cd7f98e0a31a84b3b63c2d56f8fc9f
Mukhammadkodir Abdusalomov <mukhammadkodirabdusalomov27@gmail.com>
2024-04-01 06:49:44 +0200

    username and email after error save - fixed, login.js created for login.html, disable submit-btn is not active anymore due to errors while submition, lets see if there will be an error with username and email validation in login page later on

---
+++ MoneyManager/static/js/login.js
@@ -0,0 +1,82 @@
+const usernameField = document.querySelector("#usernameField");
+const feedBackArea = document.querySelector(".invalid_feedback");
+const emailField = document.querySelector("#emailField");
+const emailFeedBackArea = document.querySelector(".emailFeedBackArea");
+const usernameSuccessOutput = document.querySelector(".usernameSuccessOutput");
+const showPasswordToggle = document.querySelector(".showPasswordToggle");
+const passwordField = document.querySelector("#passwordField");
+const submitBtn = document.querySelector(".submit-btn");
+
+// Declare global variables to track validation state
+let isUsernameValid = false;
+let isEmailValid = false;
+
+const handleToggleInput = (e) => {
+  if (showPasswordToggle.textContent === "SHOW") {
+    showPasswordToggle.textContent = "HIDE";
+    passwordField.setAttribute("type", "text");
+  } else {
+    showPasswordToggle.textContent = "SHOW";
+    passwordField.setAttribute("type", "password");
+  }
+};
+
+showPasswordToggle.addEventListener("click", handleToggleInput);
+
+emailField.addEventListener("keyup", (e) => {
+  const emailVal = e.target.value;
+  emailField.classList.remove("is-invalid");
+  emailFeedBackArea.style.display = "none";
+
+  if (emailVal.length >= 0) {
+    // Ensure validation also occurs when input is cleared
+    fetch("/authentication/validate-email", {
+      body: JSON.stringify({ email: emailVal }),
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data", data);
+        if (data.email_error) {
+          isEmailValid = false;
+          submitBtn.disabled = true;
+          emailField.classList.add("is-invalid");
+          emailFeedBackArea.style.display = "block";
+          emailFeedBackArea.innerHTML = `<p>${data.email_error}</p>`;
+        } else {
+          isEmailValid = true;
+        }
+      });
+  }
+});
+
+usernameField.addEventListener("keyup", (e) => {
+  const usernameVal = e.target.value;
+
+  usernameSuccessOutput.style.display = "block";
+  usernameSuccessOutput.textContent = `Checking ${usernameVal}`;
+
+  usernameField.classList.remove("is-invalid");
+  feedBackArea.style.display = "none";
+
+  if (usernameVal.length >= 0) {
+    // Ensure validation also occurs when input is cleared
+    fetch("/authentication/validate-username", {
+      body: JSON.stringify({ username: usernameVal }),
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data", data);
+        usernameSuccessOutput.style.display = "none";
+        if (data.username_error) {
+          isUsernameValid = false;
+          usernameField.classList.add("is-invalid");
+          feedBackArea.style.display = "block";
+          feedBackArea.innerHTML = `<p>${data.username_error}</p>`;
+        } else {
+          isUsernameValid = true;
+        }
+      });
+  }
+});
--- authentication/views.py
+++ authentication/views.py
@@ -125,3 +125,22 @@ class VerificationView(View):
 class LoginView(View):
     def get(self, request):
         return render(request, "authentication/login.html")
+
+    def post(self, request):
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        if username and password:
+            user = auth.authenticate(username=username, password=password)
+
+            if user:
+                if user.is_active:
+                    auth.login(request, user)
+                    messages.success(
+                        request, "Welcome, "+user.username+" you are now logged in to your account")
+
+                messages.error(
+                    request, "Account is not active, please check your email!")
+                return render(request, "authentication/login.html")
+            messages.error(request, "Invalid credentials, try again")
+            return render(request, "authentication/login.html")
