a0fdb3e6d30cc59718c4e5b790385d2285bab7cb
Mukhammadkodir Abdusalomov <mukhammadkodirabdusalomov27@gmail.com>
2024-04-04 10:40:21 +0200

    Pagination Success! Now you can go to next page or come back to previous page

--- expenses/views.py
+++ expenses/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Category, Expense
 from django.contrib import messages
+from django.core.paginator import Paginator
 # ! for post and get requests
 
 
@@ -9,8 +10,12 @@ from django.contrib import messages
 def index(request):
     categories = Category.objects.all()
     expenses = Expense.objects.filter(owner=request.user)
+    paginator = Paginator(expenses, 6)  # ! paginator 2
+    page_number = request.GET.get("page")
+    page_obj = Paginator.get_page(paginator, page_number)
     context = {
-        "expenses": expenses
+        "expenses": expenses,
+        "page_obj": page_obj
     }
     return render(request, "expenses/index.html", context)
 
