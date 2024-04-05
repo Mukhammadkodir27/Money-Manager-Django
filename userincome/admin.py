f8dc72c1e62b77aa1ebb6dde6f864894892eaf70
Mukhammadkodir Abdusalomov <mukhammadkodirabdusalomov27@gmail.com>
2024-04-05 13:45:04 +0200

    Admin Page style success! Income Details Stlye added!

--- userincome/admin.py
+++ userincome/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import UserIncome, Source
-# Register your models here.
 
-admin.site.register(UserIncome)
+
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'description', 'owner', 'source', 'date',)
+    search_fields = ('description', 'source', 'date',)
+
+    list_per_page = 6
+
+
+admin.site.register(UserIncome, IncomeAdmin)
 admin.site.register(Source)
